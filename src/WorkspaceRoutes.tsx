86657feccba1036d1307d0bdbd51ca06e758574d
Johan Bové <johan@johanbove.info>
2022-04-16 22:30:42 +0200

    Fixes GH Issue #10 Proposal to show "AddForm" when a given share name is not found

--- src/WorkspaceRoutes.tsx
+++ src/WorkspaceRoutes.tsx
@@ -4,12 +4,27 @@ import ThreadReplyForm from "./ThreadReplyForm";
 import ThreadView from "./ThreadView";
 import ThreadListing from "./ThreadListing";
 import SpaceSettings from "./SpaceSettings";
+import { useWorkspaceAddrFromRouter } from "./WorkspaceLookup";
+import AddForm from "./AddForm";
 
 export default function WorkspaceRoutes() {
+  const workspace = useWorkspaceAddrFromRouter();
+
+  const ShareNotFound = function () {
+    return (<AddForm />);
+  }
+
+  /**
+   * If a workspace is found, load it, otherwise show "add form" 
+   */
+  const ThreadListingElement = function () {
+    return workspace ? (<ThreadListing />) : (<ShareNotFound />);
+  };
+
   return (
     <Routes>
       <Route path={"/settings"} element={<SpaceSettings />} />
-      <Route path={"/"} element={<ThreadListing />}>
+      <Route path={"/"} element={<ThreadListingElement />}>
         <Route path={"post"} element={<NewThreadForm />} />
         <Route
           path={"thread/:authorPubKey/:timestamp"}
